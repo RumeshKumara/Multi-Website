e2a10d3349a69bac35424e3e7b0dc6af2a428413
rumesh <rumeshk066@gmail.com>
2025-03-05 13:51:10 +0530

    create profile card

---
+++ script.js
@@ -0,0 +1,12 @@
+let sum;
+console.log(sum);
+
+let a = 5;
+let b = '5';
+
+if (a === b) {
+    console.log('a and b are equal in value and type');
+} else {
+    console.log('a and b are not equal in value or type');
+}
+
